8de32a1869fad21eb415928ec63902a99d0b03a0
ViacheslavL <realwenzel@gmail.com>
2017-04-10 21:36:21 +0300

    Remove redundant code

--- include/xlsxwriter/hash_table.hpp
+++ include/xlsxwriter/hash_table.hpp
@@ -12,28 +12,10 @@
 
 #include "common.hpp"
 #include <list>
-#include <stdint.h>
+#ifndef INT16_MAX
+#define INT16_MAX (32767)
+#endif
 #include <unordered_map>
-
-/* Macro to loop over hash table elements in insertion order. */
-#define LXW_FOREACH_ORDERED(elem, hash_table) \
-    STAILQ_FOREACH((elem), (hash_table)->order_list, lxw_hash_order_pointers)
-
-/* List declarations. */
-STAILQ_HEAD(lxw_hash_order_list, lxw_hash_element);
-SLIST_HEAD(lxw_hash_bucket_list, lxw_hash_element);
-
-/* LXW_HASH hash table struct. */
-typedef struct lxw_hash_table {
-    uint32_t num_buckets;
-    uint32_t used_buckets;
-    uint32_t unique_count;
-    uint8_t free_key;
-    uint8_t free_value;
-
-    struct lxw_hash_order_list *order_list;
-    struct lxw_hash_bucket_list **buckets;
-} lxw_hash_table;
 
 /*
  * LXW_HASH table element struct.
@@ -42,14 +24,10 @@ typedef struct lxw_hash_table {
  * lists in the the hash table buckets and also pointers to track the
  * insertion order in a separate list.
  */
-typedef struct lxw_hash_element {
+struct lxw_hash_element {
     void *key;
     void *value;
-
-    STAILQ_ENTRY (lxw_hash_element) lxw_hash_order_pointers;
-    SLIST_ENTRY (lxw_hash_element) lxw_hash_list_pointers;
-} lxw_hash_element;
-
+};
 
 size_t _generate_hash_key(void *data, size_t data_len, size_t num_buckets);
 
@@ -59,15 +37,6 @@ size_t generate_hash_key(const T* data)
     return _generate_hash_key((void*)data, sizeof(T), INT16_MAX);
 }
 size_t generate_hash_key(const std::string& data);
-
-
-lxw_hash_element *lxw_hash_key_exists(lxw_hash_table *lxw_hash, void *key,
-                                      size_t key_len);
-lxw_hash_element *lxw_insert_hash_element(lxw_hash_table *lxw_hash, void *key,
-                                          void *value, size_t key_len);
-lxw_hash_table *lxw_hash_new(uint32_t num_buckets, uint8_t free_key,
-                             uint8_t free_value);
-void lxw_hash_free(lxw_hash_table *lxw_hash);
 
 namespace xlsxwriter {
 
